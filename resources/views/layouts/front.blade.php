a320b1c1ce0dcd18065cfa958c1b9698f2390168
Rojifa-Akhter <mstrojifaakhter@gmail.com>
2026-06-24 18:02:07 +0600

    fix title

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Bhaiya Housing')</title>
     <link rel="icon" type="image/webp" href="{{ asset('assets/images/fav.webp') }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
